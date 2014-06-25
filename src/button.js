ec50518545d4a4397d46378bda0ba11fc27c66e3
Mingi-Seo <lg77126266@gmail.com>
2014-06-26 06:08:25 +0900

    Update button.js

--- src/button.js
+++ src/button.js
@@ -1,54 +1,25 @@
-var onFullscreen = 0;
-
-function buttonInit()
-{
-	var temp = "<div class='btn-group'>\
-					<button type='button' id='newNote' class='btn btn-default btn-sm btn-small' onclick='clickNewnote();'>\
-						<i class='glyphicon glyphicon-asterisk'></i></button>\
-					<button type='button' id='fullScreen' class='btn btn-default btn-sm btn-small' onclick='clickFullscreen();'>\
-						<i class='glyphicon glyphicon-fullscreen'></i></button>\
-					<button type='button' id='saveHTML' class='btn btn-default btn-sm btn-small' onclick='clickSaveHTML();'>\
-						<i class='glyphicon glyphicon-floppy-disk'></i></button>\
-				</div>";
-
-	$('.note-toolbar.btn-toolbar').append(temp);
-}
+var viewFullscreen = 0;
 
 function getDom(id)
 {
-    return document.getElementById(id);
+	return document.getElementById(id);
 }
 
-function currentStateDoc()
+function clickNewnote()
 {
-    var docElm = "<!doctype html>\
-                <html>\
-                    <head>\
-                        <meta charset='utf-8'>\
-                        <title> My Note </title>\
-                    </head>\
-                    <body>"
-                + getHtmlCode(); +
-                    "</body>\
-                </html>";
+	setHtmlCode("");
 
-    return docElm;
+	autoSave();
 }
 
-function clickFullscreen()
+function clickToggleFullscreen()
 {
-    var viewFullScreen = getDom("fullScreen");
+	if (!viewFullscreen)
+	{
+		var docElm = document.documentElement;
 
-    $('.note-editor').attr("id", "screen");
-    $('.note-statusbar').attr("id", "bar");
-    
-    if (onFullscreen == 0)
-    {
-        // var docElm = document.documentElement;
-        var docElm = getDom("screen");
-
-        getDom("bar").className = "hide";
-        onFullscreen = 1;
+		getDom("fullscreen").innerHTML = "Cancle Fullscreen";
+		viewFullscreen = 1;
 
         if (docElm.requestFullscreen)
         {
@@ -71,22 +42,14 @@ function clickFullscreen()
         }
 
         docElm.webkitRequestFullScreen(Element.ALLOW_KEYBOARD_INPUT);
+	}
 
-        $(window).resize(function()
-        {
-            $('#screen').width($(window).width());
-            $('#screen').height($(window).height());
-            $('.note-editable').width($(window).width());
-            $('.note-editable').height($(window).height());
-        });
-    }
+	else if (viewFullscreen)
+	{
+		getDom("fullscreen").innerHTML = "Fullscreen";
+		viewFullscreen = 0;
 
-    else if (onFullscreen == 1)
-    {
-        getDom("bar").className = "note-statusbar";
-    	onFullscreen = 0;
-
-        if (document.exitFullscreen)
+	    if (document.exitFullscreen)
         {
             document.exitFullscreen();
         }
@@ -105,14 +68,23 @@ function clickFullscreen()
         {
             document.webkitCancelFullScreen();
         }
-    }
+	}
 }
 
-function clickNewnote()
+function currentStateDoc()
 {
-    setHtmlCode("");
+    var docElm = "<!doctype html>\
+                <html>\
+                    <head>\
+                        <meta charset='utf-8'>\
+                        <title> My Note </title>\
+                    </head>\
+                    <body>"
+                + getHtmlCode(); +
+                    "</body>\
+                </html>";
 
-    autoSave();
+    return docElm;
 }
 
 function clickSaveHTML()
@@ -132,5 +104,3 @@ function clickSaveHTML()
 
     saveAs(new Blob([docElm], {type: "text/plain;charset=" + charset}), html_filename);
 }
-
-buttonInit();
